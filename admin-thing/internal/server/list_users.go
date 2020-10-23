bad6472ae8800e9fc0303d2fee3605b6563dd946
Adam Cooper <adam.cooper@digital.justice.gov.uk>
2020-10-23 17:36:36 +0100

    Can now type a search query

--- admin-thing/internal/server/list_users.go
+++ admin-thing/internal/server/list_users.go
@@ -9,6 +9,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
 )
 
 const ListUsersRoute = "/users"
@@ -21,36 +22,52 @@ type User struct {
 // ListUsersHandler shows a list of all users registered
 func ListUsersHandler(tmpl Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		sess := session.Must(session.NewSessionWithOptions(session.Options{
-			SharedConfigState: session.SharedConfigEnable,
-		}))
-
-		svc := dynamodb.New(sess, &aws.Config{Endpoint: aws.String("http://localhost:8000")})
-
-		params := &dynamodb.ScanInput{
-			TableName: aws.String("ActorUsers"),
-		}
-
-		// Make the DynamoDB Query API call
-		result, err := svc.Scan(params)
-		if err != nil {
-			fmt.Println("Query API call failed:")
-			fmt.Println((err.Error()))
-		}
-
 		var users []User
 
-		for _, i := range result.Items {
-			user := User{}
+		if r.Method == http.MethodPost {
+			filt := expression.Name("Email").Contains(r.FormValue("email"))
+			proj := expression.NamesList(expression.Name("Email"))
 
-			err = dynamodbattribute.UnmarshalMap(i, &user)
-
+			expr, err := expression.NewBuilder().WithFilter(filt).WithProjection(proj).Build()
 			if err != nil {
-				fmt.Println("Got error unmarshalling:")
+				fmt.Println("Got error building expression:")
 				fmt.Println(err.Error())
 			}
 
-			users = append(users, user)
+			sess := session.Must(session.NewSessionWithOptions(session.Options{
+				SharedConfigState: session.SharedConfigEnable,
+			}))
+
+			svc := dynamodb.New(sess, &aws.Config{Endpoint: aws.String("http://localhost:8000")})
+
+			params := &dynamodb.ScanInput{
+				ExpressionAttributeNames:  expr.Names(),
+				ExpressionAttributeValues: expr.Values(),
+				FilterExpression:          expr.Filter(),
+				ProjectionExpression:      expr.Projection(),
+				TableName:                 aws.String("ActorUsers"),
+			}
+
+			// Make the DynamoDB Query API call
+			result, err := svc.Scan(params)
+			if err != nil {
+				fmt.Println("Query API call failed:")
+				fmt.Println((err.Error()))
+			}
+
+			for _, i := range result.Items {
+				user := User{}
+
+				err = dynamodbattribute.UnmarshalMap(i, &user)
+
+				if err != nil {
+					fmt.Println("Got error unmarshalling:")
+					fmt.Println(err.Error())
+				}
+
+				users = append(users, user)
+			}
+
 		}
 
 		data := struct {
