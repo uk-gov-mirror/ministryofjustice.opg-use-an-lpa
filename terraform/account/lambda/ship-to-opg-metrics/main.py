6c49410f68bf1980bdd588764da130fef8627ddc
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2021-03-17 09:37:53 +0000

    clean up variables

--- terraform/account/lambda/ship-to-opg-metrics/main.py
+++ terraform/account/lambda/ship-to-opg-metrics/main.py
@@ -6,11 +6,11 @@ def handler(event, context):
     for message in event['Records']:
       call_api_gateway(str(message))
 
-def call_api_gateway(self, json_data):
+def call_api_gateway(json_data):
     url = os.getenv('OPG_METRICS_URL')
     api_key = os.getenv('API_KEY')
     method = 'PUT'
-    path = '/{}/metrics'.format(self.api_gateway_stage)
+    path = '/development/metrics'
     headers = {
       'Content-Type': 'application/json',
       'Content-Length': str(len(str(json_data))),
@@ -20,7 +20,6 @@ def call_api_gateway(self, json_data):
     response = requests.request(
         method=method,
         url=url+path,
-        auth=self.aws_auth,
         json=json_data,
         headers=headers
     )
